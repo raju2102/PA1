6c61a7eb03b8804dda1d6bd59057ff4d837cd6b1
Raja karthik Vobugari <raju@tamu.edu>
2024-09-02 11:51:57 -0500

    Added code for max_2_sum

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -11,6 +11,21 @@ end
 
 def max_2_sum(arr)
   # YOUR CODE HERE
+  if arr.size == 0
+    return 0
+  elsif arr.size == 1
+    return arr[0]
+  end
+  max1, max2 = arr[0] > arr[1] ? [arr[0], arr[1]] : [arr[1], arr[0]]
+  arr[2..-1].each do |a|
+    if a > max1
+      max2 = max1
+      max1 = a
+    elsif a > max2
+      max2 = a
+    end
+  end
+  return max1 + max2
 end
 
 def sum_to_n?(arr, number)
